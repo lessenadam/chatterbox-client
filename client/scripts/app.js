796dd258a72d35c2f73181f65f160fe5bee478d6
lessenadam <lessena@gmail.com>
2016-05-02 17:21:06 -0700

    Solve all test specs
    - Create basic app structure
    - Forms, submits, click handlers, etc

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,40 +1,84 @@
 // YOUR CODE HERE:
-var app = {};
+var app = {
+  init: function() {
+    $(document).ready(function () {
+      $('.username').unbind();
+      $('.username').on('click', function() {
+        console.log('click username event triggered');
+        app.addFriend();
+      });
+      $('#send .submit').unbind();
+      $('#send .submit').on('submit', function() {
+        console.log('submit event triggered');
+        app.handleSubmit();
+      });
+    });
+  },
 
-app.init = function() {};
+  send: function(message) {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'https://api.parse.com/1/classes/messages',
+      data: JSON.stringify(message),
+      type: 'POST',
+      contentType: 'application/json',
+      success: function (data) {
+        console.log("Message pushed successfully");  
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to get data', data);
+      }
+    });
+  },
 
-app.send = function(message) {
-  $.ajax({
-    // This is the url you should use to communicate with the parse API server.
-    url: 'https://api.parse.com/1/classes/messages',
-    data: JSON.stringify(message),
-    type: 'POST',
-    contentType: 'application/json',
-    success: function (data) {
-      console.log("Message pushed successfully");  
-    },
-    error: function (data) {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to get data', data);
-    }
-  });
+  fetch: function(message) {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      // url: 'https://api.parse.com/1/classes/messages',
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+        displayMessages(data);
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to get data', data);
+      }
+    });
+  },
+
+  clearMessages: function() {
+    $('#chats').children().remove();
+  },
+
+  addMessage: function(message) {
+    var text = message.text;
+    var user = message.username;
+    var room = message.roomname;
+    var $message = `<span>User: <span class='username'>${user}</span> <br> Text: ${text} <br> Room: ${room} <br></span>`;
+    $('#chats').append($message);
+  },
+  addRoom: function(room) {
+    $room = `<span> ${room} </span>`;
+    $('#roomSelect').append($room);
+  },
+  addFriend: function() {
+    // do soemthing\
+    console.log('addFriend called');
+  },
+
+  handleSubmit: function() {
+    console.log('handleSubmit called');
+  }
+
 };
 
-app.fetch = function(message) {
-  $.ajax({
-    // This is the url you should use to communicate with the parse API server.
-    // url: 'https://api.parse.com/1/classes/messages',
-    type: 'GET',
-    contentType: 'application/json',
-    success: function (data) {
-      displayMessages(data);
-    },
-    error: function (data) {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to get data', data);
-    }
-  });
-};
+
+
+
+
+
 
 
 var displayMessages = function(data) {
@@ -54,7 +98,6 @@ var displayMessages = function(data) {
       user = user.replace(/'/g, "&apos");
       user = user.replace(/"/g, "&quot");
     }
-
     var $message = `<span>User: ${user} <br> ${text}</span>`;
     $('#chats').append($message);
   });
